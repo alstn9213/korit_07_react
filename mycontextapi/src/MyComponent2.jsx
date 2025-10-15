fa9f3b48b35c9e6c31c99f304e5af0abd13b4088
alstn9213 <alstn9213@naver.com>
2025-10-15 16:11:19 +0900

    feat:20251015

---
+++ mycontextapi/src/MyComponent2.jsx
@@ -0,0 +1,12 @@
+function MyComponent2() {
+  // 버튼을 눌렀을 때 호출되는 함수를 하나 정의하겠다.
+  const handleClick = () => {
+    alert('Button pressed !');
+  }
+  return (
+    <>
+      <button onClick={handleClick}>Press Me !</button>
+    </>
+  );
+}
+export default MyComponent2
