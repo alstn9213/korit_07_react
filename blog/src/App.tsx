9cfd8cba0286ce7a1f69b6a5798b7820274ee92e
alstn9213 <alstn9213@naver.com>
2025-10-27 16:33:19 +0900

    '20251027'

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-
+import { Route, Routes } from 'react-router'
 import './App.css'
+import RootLayout from './layouts/RootLayout'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import Write from './pages/Read'
+import Read from './pages/Read'
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home/>}/>
+          <Route path='/auth' element={<Auth/>}/>
+          <Route path='/write' element={<Write/>}/>
+          <Route path='/read/:id' element={<Read/>}/>
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
