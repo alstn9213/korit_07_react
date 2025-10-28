2e951562706704146e7bc2eeaf0e897eaf5052fa
alstn9213 <alstn9213@naver.com>
2025-10-28 13:46:41 +0900

    '20251028blog'

--- blog/src/pages/Auth.tsx
+++ blog/src/pages/Auth.tsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+
 export default function Auth() {
-  return <div>Auth</div>;
+  const [pageType, setPageType] = useState('login');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [username, setUsername] = useState('');
+
+  const handlePageChange = (type: string) => {
+    setEmail('');
+    setPassword('');
+    setPasswordConfirm('');
+    setUsername('');
+    setPageType(type);
+  };
+
+  return <main className="page__main">
+    <article>
+      <section>
+        <nav>
+          <button>
+            
+          </button>
+          <button>
+            
+          </button>
+        </nav>
+      </section>
+    </article>
+
+  </main>
 }
